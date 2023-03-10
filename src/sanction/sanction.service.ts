8f6dc40730640e22a1d0f7a168bf31476ce17e4d
PaulUno777 <pauluno777@gmail.com>
2023-03-10 16:50:50 +0100

    sanction feature ready

--- src/sanction/sanction.service.ts
+++ src/sanction/sanction.service.ts
@@ -1,15 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class SanctionService {
+  private readonly logger = new Logger(SanctionService.name);
   constructor(private prisma: PrismaService) {}
   async findAll() {
+    this.logger.log('Getting sanction list ...');
     const sanctions = await this.prisma.sanctionList.findMany({
       orderBy: {
-        name: 'desc',
+        name: 'asc',
       },
     });
+    this.logger.log('(success !) all is well');
     return sanctions.map((item) => {
       return {
         id: item.id,
