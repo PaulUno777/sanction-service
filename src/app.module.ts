bf389dc860da4f838290d75986c7e025aef565eb
PaulUno777 <78661160+PaulUno777@users.noreply.github.com>
2023-06-09 11:46:26 +0100

    fix statics folders issues

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { SanctionedModule } from './sanctioned/sanctioned.module';
 import { SearchModule } from './search/search.module';
 import { SanctionModule } from './sanction/sanction.module';
 import { ScheduleModule } from '@nestjs/schedule';
-import { existsSync, mkdirSync, rmSync } from 'fs';
+import { existsSync, mkdirSync } from 'fs';
 import { join } from 'path';
 
 @Module({
@@ -22,21 +22,7 @@ import { join } from 'path';
   ],
   providers: [],
 })
-export class AppModule implements NestModule, OnModuleInit {
-  onModuleInit() {
-    const SOURCE_DIR = './sanctions_source/';
-    const PUBLIC_DIR = './public/'
-    
-    //manage source directory
-    if (!existsSync(join(process.cwd(), PUBLIC_DIR))) {
-      mkdirSync(join(process.cwd(), PUBLIC_DIR));
-      console.log('public directory created');
-    }
-    if (!existsSync(join(process.cwd(), SOURCE_DIR))) {
-      mkdirSync(join(process.cwd(), SOURCE_DIR));
-      console.log('sanction source directory created');
-    }
-  }
+export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes('*');
   }
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,28 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { existsSync, mkdirSync } from 'fs';
+import { join } from 'path';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.setGlobalPrefix('api');
+
+  //create my folder 
+  const SOURCE_DIR = './sanctions_source/';
+    const PUBLIC_DIR = './public/'
+    
+    //manage source directory
+    if (!existsSync(join(process.cwd(), PUBLIC_DIR))) {
+      mkdirSync(join(process.cwd(), PUBLIC_DIR));
+      console.log('public directory created');
+    }
+    if (!existsSync(join(process.cwd(), SOURCE_DIR))) {
+      mkdirSync(join(process.cwd(), SOURCE_DIR));
+      console.log('sanction source directory created');
+    }
 
   //Open API Documentation
   const config = new DocumentBuilder()
